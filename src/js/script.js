3d07701e1f4c45b59c2ddd6d76d460ce32d844af
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-21 11:51:19 -0300

    Variaveis,declarações e tipos

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+//DECLARAÇÃO 
+var nome = "Fiap"
+console.log(typeof nome)
+
+
+let idade =24;
+console.log(typeof idade)
+
+
+const dev = true
+console.log(typeof dev)
+
+
+let dados = ["um","dois"]
+console.log(typeof dados)
